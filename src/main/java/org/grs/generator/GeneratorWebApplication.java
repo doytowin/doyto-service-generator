a822bbe24c45062d8307911f9db81dd062ed7323
袁臻 <f0rb@163.com>
2016-08-24 16:31:09 +0800

    Hikari配置更新

--- src/main/java/org/grs/generator/GeneratorWebApplication.java
+++ src/main/java/org/grs/generator/GeneratorWebApplication.java
@@ -17,20 +17,16 @@
 package org.grs.generator;
 
 import javax.servlet.Filter;
-import javax.sql.DataSource;
 
 import org.grs.generator.common.WebContextFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GeneratorWebApplication extends SpringBootServletInitializer {
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(GeneratorWebApplication.class, args);
     }
@@ -40,15 +36,8 @@ public class GeneratorWebApplication extends SpringBootServletInitializer {
         return application.sources(GeneratorWebApplication.class);
     }
 
-
     @Bean
     public Filter configWebContextFilter() {
         return new WebContextFilter();
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
     }
 }
